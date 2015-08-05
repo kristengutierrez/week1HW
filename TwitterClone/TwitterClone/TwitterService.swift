87817d73367b771313ecd2e4b718020779d57b05
kristenkozmary <kristenkozmary@yahoo.com>
2015-08-05 10:51:39 -0700

    new commit

--- TwitterClone/TwitterClone/TwitterService.swift
+++ TwitterClone/TwitterClone/TwitterService.swift
@@ -12,56 +12,26 @@ import Social
 
 
 class TwitterService {
-  class func tweetsFromHomeTimeline
+  class func tweetsFromHomeTimeline(account : ACAccount, completionHandler : (String?, [Tweet]?) -> (Void)) {
+    let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")!, parameters: nil)
+    request.account = account
+    request.performRequestWithHandler { (data, response, error) -> Void in
+      if let error = error {
+        completionHandler("could not connect to server", nil)
+      } else {
+        println(response.statusCode)
+        switch response.statusCode {
+        case 200...299:
+          let tweets = TweetJSONParser.tweetsFromJSONData(data)
+          completionHandler(nil,tweets)
+        case 400...499:
+          completionHandler("Our fault", nil)
+        case 500...599:
+          completionHandler("Server's fault", nil)
+        default:
+          completionHandler("Something went wrong", nil)
+        }
+      }
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class TwitterService {
-//  class func tweetsFromHomeTimeline(account : ACAccount, completionHandler : (String?, [Tweet]?) -> (Void)) {
-//    let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")!, parameters: nil)
-//    request.account = account
-//    request.performRequestWithHandler { (data, response, error) -> Void in
-//      if let error = error {
-//        completionHandler("could not connect to the server", nil)
-//      } else {
-//        println(response.statusCode)
-//        switch response.statusCode {
-//        case 200...299:
-//          let tweets = TweetJSONParser.tweetsFromJSONData(data)
-//          completionHandler(nil,tweets)
-//        case 400...499:
-//          completionHandler("this is our fault", nil)
-//        case 500...599:
-//          completionHandler("this is the server's fault", nil)
-//        default:
-//          completionHandler("error occurred", nil)
-//        }
-//      }
-//    }
-//  }
-//}
