98eed08c3430bfa3f3d0e325fba3e1778e44f585
kristenkozmary <kristenkozmary@yahoo.com>
2015-08-10 08:41:02 -0700

    playground changed

--- Week1Day1CodingChallenge.playground/Contents.swift
+++ Week1Day1CodingChallenge.playground/Contents.swift
@@ -12,3 +12,75 @@ for num in arr {
   reversedarr.insert(num, atIndex: 0)
 }
 println(reversedarr)
+
+var numbers = [Int]()
+numbers += 1...100
+
+for number in numbers {
+  switch number{
+  case _ where number % 3 == 0 && number % 5 == 0:
+    println("FizzBuzz")
+  case _ where number % 3 == 0:
+    println("Fizz")
+  case _ where number % 5 == 0:
+    println("Buzz")
+  default:
+    println(number)
+    }
+}
+var myString = "hi one hi two hi hi three hi four five hi"
+
+var newArray = myString.componentsSeparatedByString("hi")
+var numberss = (newArray.count - 1)
+println(numberss)
+
+//var search = "hi"
+//var length = myString.count
+
+
+//http://waynewbishop.com/swift/stacks-queues/ learning how to implement queues
+//
+
+class QNode<T> {
+  var key: T?
+  var next: QNode?
+}
+
+
+public class Queue<T> {
+  private var top : QNode<T>! = QNode<T>()
+  func enQueue(var key: T) {
+    if (top == nil) {
+      top = QNode<T>()
+    }
+    if (top.key == nil) {
+      top.key = key
+      return
+    }
+  
+  var item: QNode<T> = QNode<T>()
+  var current: QNode<T> = top
+  while (current.next != nil) {
+    current = current.next!
+  }
+  item.key = key
+  current.next = item
+  }
+  func deQueue() -> T? {
+    let topItem: T? = self.top?.key
+    if (topItem == nil){
+      return nil
+    }
+    var queueItem: T? = top.key!
+    if let nextItem = top.next {
+      top = nextItem
+    } else {
+      top = nil
+    }
+    return queueItem
+  }
+}
+
+
+
+
